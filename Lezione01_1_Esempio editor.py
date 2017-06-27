f69e6536d83c7e78663250ae32e5acb57dde8775
Marco Schaerf <marco.schaerf@uniroma1.it>
2017-06-27 12:01:20 +0200

    Create Lezione01_1_Esempio editor.py

---
+++ Lezione01_1_Esempio editor.py
@@ -0,0 +1,10 @@
+#questo è l'editor di file di Python
+#tutte le istruzioni vengono eseguite insieme (F5 o Run->Run module)
+#tutte le righe che cominciano con # vengono ignorate (commenti)
+#NON mostra il risultato delle espressioni --> per vedere il risultato DOVETE usare print
+#fate caso ai colori
+
+print("Ciao")
+print(5+3)
+8+8
+ 
